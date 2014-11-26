600247afeeb27b00fbef118158c4d17103fcfe0a
caneroj1 <caneroj1@tcnj.edu>
2014-11-26 11:21:52 -0500

    updated code to use command-line arguments for width and height if present.

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,4 +1,5 @@
 require_relative 'game_window.rb'
 
-window = GameWindow.new(480, 480)
+window = GameWindow.new(ARGV[0] ? ARGV[0].to_i : 480,
+                        ARGV[1] ? ARGV[1].to_i : 480)
 window.show
